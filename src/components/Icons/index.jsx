e51ed4b1d97fdfa8518c37a10ea207593cd9b1e8
TomaszFeszter <tomek.feszter.grupa@gmail.com>
2022-07-17 18:44:04 +0200

    import icons

---
+++ src/components/Icons/index.jsx
@@ -0,0 +1,15 @@
+export { ReactComponent as Logo } from "./icons/logo/Logomark.svg";
+export { ReactComponent as LogoCircle } from "./icons/logo/LogomarkCircle.svg";
+export { ReactComponent as LogoHorizontal } from "./icons/logo/LogotypeHorizontal.svg";
+export { ReactComponent as LogoVertical } from "./icons/logo/LogotypeVertical.svg";
+
+export { ReactComponent as Heart } from "./icons/heart.svg";
+export { ReactComponent as Cart } from "./icons/buy.svg";
+export { ReactComponent as Messages } from "./icons/chat.svg";
+export { ReactComponent as Notification } from "./icons/notification.svg";
+export { ReactComponent as Paper } from "./icons/paper.svg";
+export { ReactComponent as Profile } from "./icons/profile.svg";
+export { ReactComponent as Search } from "./icons/search.svg";
+export { ReactComponent as Settings } from "./icons/settings.svg";
+export { ReactComponent as Star } from "./icons/star.svg";
+export { ReactComponent as Work } from "./icons/work.svg";
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Heading from "./components/Heading";
+import { Heart, Logo } from "./components/Icons";
 import "./sass/main.scss";
 
 export default function App() {
   return (
     <div>
       <Heading>
+        <Logo />
+        <Heart width={100} height={100} />
         testowy <Heading>heading</Heading> stworzony za pomoca reacta
       </Heading>
     </div>
