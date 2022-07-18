c32bf4f1b286d406d8dca9236495b83a9c5239ca
TomaszFeszter <tomek.feszter.grupa@gmail.com>
2022-07-19 00:17:48 +0200

    included menu and app navigation

---
+++ src/components/Menu/index.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { CloseButton } from "../BackButton";
+import Button from "../Button";
+import { Paper, Profile } from "../Icons";
+import SubHeading from "../SubHeading";
+
+const Menu = ({ userName, userEmail, userImg, open, handleClose }) => {
+  return (
+    <div
+      onBlur={handleClose}
+      tabIndex={0}
+      className={`menu ${open ? "menu--active" : ""} pl-10 pt-16`}
+    >
+      <CloseButton onClick={handleClose} />
+      <section className="menu__user mb-20">
+        <figure className="menu__user__avatar mb-6">
+          <img src={userImg} alt="user avatar" />
+        </figure>
+        <SubHeading>{userName}</SubHeading>
+        <SubHeading size="small">{userEmail}</SubHeading>
+      </section>
+      <ul className="menu__options">
+        <li className="menu__options__item">
+          <Link className="link link--black" to="/account">
+            <Profile /> My profile
+          </Link>
+        </li>
+        <li className="menu__options__item">
+          <Link className="link link--black" to="/order-history">
+            <Paper /> Order history
+          </Link>
+        </li>
+      </ul>
+      <Button>Log out</Button>
+    </div>
+  );
+};
+
+export default Menu;
--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,19 +1,11 @@
 import React from "react";
-import Link from "../../components/Links";
-import ProductListItem from "../../components/ProductListItem";
+import AppNavigation from "../../components/AppNavigation";
+import Menu from "../../components/Menu";
 
 const Home = () => {
   return (
     <div>
-      Home <Link href="/products/some-id">Go to product page</Link>
-      <ProductListItem
-        imgSrc={
-          "https://www.pngall.com/wp-content/uploads/2016/05/Burger-PNG-Image.png"
-        }
-        caption="The Macdonalds"
-        description="Classic cheesburger"
-        price="$23.99"
-      />
+      <AppNavigation /> <Menu />
     </div>
   );
 };
