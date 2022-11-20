0e4438618fc62ab56f468cab303f3b99d104acd3
TomaszFeszter <tomek.feszter.grupa@gmail.com>
2022-11-20 17:04:49 +0100

    changes in layout

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,23 +1,5 @@
 import React from "react";
 
-export const Container = ({ children }) => {
-  return <div className="container pl-10 pr-10">{children}</div>;
-};
-
-export const Aside = ({ children }) => {
-  return <aside className="aside">{children}</aside>;
-};
-
-export const Main = ({ children }) => {
-  return <div className="main">{children}</div>;
-};
-
-export const Layout = ({ children, center }) => {
-  return (
-    <div className={`layout ${center ? "layout--center" : ""}`}>{children}</div>
-  );
-};
-
 export const Page = ({ children, modifier = "" }) => {
   return <div className={`container pl-10 pr-10 ${modifier}`}>{children}</div>;
 };
--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -7,35 +7,27 @@ import { Page } from "../../layouts";
 import foodImage from "./images/food.png";
 
 const Home = () => {
-  const firstColumn = (
-    <div className="flex-column pt-16">
-      <Logo className="logo mb-16" />
-      <Heading size={"big"} styles={"mb-16"}>
-        Choose from a wide range of delicious meals
-      </Heading>
-      <img src={foodImage} alt="Some food" />
-    </div>
-  );
-
-  const secondColumn = (
-    <div className="flex-column">
-      <Link to="/sign-up">
-        <Button long styles={"mb-10"}>
-          Create account
-        </Button>
-      </Link>
-      <Link to="/log-in">
-        <Button secondary long>
-          Login
-        </Button>
-      </Link>
-    </div>
-  );
-
   return (
     <Page>
-      {firstColumn}
-      {secondColumn}
+      <div className="flex-column pt-16">
+        <Logo className="logo mb-16" />
+        <Heading size={"big"} styles={"mb-16"}>
+          Choose from a wide range of delicious meals
+        </Heading>
+        <img src={foodImage} alt="Some food" />
+      </div>
+      <div className="flex-column">
+        <Link to="/sign-up">
+          <Button long styles={"mb-10"}>
+            Create account
+          </Button>
+        </Link>
+        <Link to="/log-in">
+          <Button secondary long>
+            Login
+          </Button>
+        </Link>
+      </div>
     </Page>
   );
 };
